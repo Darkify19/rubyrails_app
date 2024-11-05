6d297acf1bc8a916d74315adf2b560f5f0802e14
Janzell <94756559+Darkify19@users.noreply.github.com>
2024-11-05 14:27:32 +0800

    removed static pages, added auto title to all
    removed rails generate static pages. proceeded with chapter 4.

--- test/controllers/render_controller_test.rb
+++ test/controllers/render_controller_test.rb
@@ -4,5 +4,6 @@ class RenderControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get render_index_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 end
