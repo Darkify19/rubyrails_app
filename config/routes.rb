c24ba89da5bc7f57a3618d5db90c6510869dd1fa
Janzell <94756559+Darkify19@users.noreply.github.com>
2024-11-08 00:26:32 +0800

    corrected and cleared static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   get "render/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -13,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "render#index"
+  root 'static_pages#home'
 end
