79c947550c749693ff7e6f0957b337439ec09041
Janzell <94756559+Darkify19@users.noreply.github.com>
2024-11-13 17:26:37 +0800

    updated timestamp, fixed error

--- db/schema.rb
+++ db/schema.rb
@@ -20,5 +20,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_12_075106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
